67e0a3e9bea85f1f87f1e7a02ed452cb801a0bd8
Erica Frisch <frisch.erica@gmail.com>
2020-01-24 23:58:46 -0800

    Added pie charts

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 var neighborhoodsURL = "https://opendata.arcgis.com/datasets/1ef75e34b8504ab9b14bef0c26cade2c_3.geojson"
 
-function createMap(neighborhoodData, airbnbData) {
+function createMap(neighborhoodData, airbnbData,roomData) {
 
     var neighborhoods = createFeatures(neighborhoodData);
     var airbnbListings = createClusters(airbnbData);
@@ -27,11 +27,110 @@ function createMap(neighborhoodData, airbnbData) {
         "Airbnb Listings": airbnbListings
     }
 
+    var percents = {}
+    roomData.forEach(room => {
+        var hood = room.neighbourhood;
+        var room_type = room.room_type;
+        if (percents[hood]){
+            percents[hood][room_type] = room.percent*100;
+        } else {
+            percents[hood] = {};
+            // percents[hood]['Entire home/apt'] = 0;
+            // percents[hood]['Private room'] = 0;
+            // percents[hood]['Shared room'] = 0;
+            // percents[hood]['Hotel room'] = 0;
+            percents[hood][room_type] = room.percent*100;
+            percents[hood]["lat"] = room.latitude;
+            percents[hood]["long"] = room.longitude;
+        };
+    });
+    
+    var pieLayers = new L.LayerGroup();
+    var pies = [];
+    Object.entries(percents).forEach(([index,hood]) => {
+        var rooms = []
+        if (hood['Entire home/apt']) {
+            rooms.push({num: hood['Entire home/apt'], label: "Entire home/apt"})
+        } 
+        if (hood["Private room"]) {
+            rooms.push({num: hood["Private room"], label: "Private room"})
+        }
+        if (hood['Shared room']) {
+            rooms.push({num: hood['Shared room'], label: "Shared room"})
+        }
+        if (hood["Hotel room"]) {
+            rooms.push({num: hood["Hotel room"], label: "Hotel room"})
+        }
+        var new_pie = L.pie([hood['lat'],hood['long']], rooms).bindPopup(`${index}`);
+    pies.push([index,new_pie]);
+    });
+
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
       }).addTo(map);
     
+    options = ''
+    pies.forEach(pie =>{
+        options += `<option id="pie-option" value=${pie[0].replace(" ","_")}>${pie[0]}</option>`;
+        // pieLayers.addLayer(pie[0])
+    });
+    
+
+    var legend = L.control({position: 'topright'});
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend');
+        div.innerHTML = '<select><option value="none">Select A Neighbourhood</option>'+options+'</select>';
+        div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+        return div;
+    };
+    legend.addTo(map);
+    
     map.addLayer(airbnbListings);
+    // pies.forEach(pie => map.addLayer(pie[1]));
+    
+    var current_pie = ''
+    d3.select("select").on("change",function(event) {
+        var selected = d3.select(this)
+            // .select("#pie-option")
+            .property("value");
+        
+        console.log(selected);
+        pies.forEach(pie => {
+            // map.clearLayers();
+            
+            if (pie[0].replace(" ","_") === selected) {
+                map.addLayer(pie[1]);
+            }
+            if (pie[0].replace(" ","_") === current_pie) {
+                map.removeLayer(pie[1]);
+            }
+            // if (selected !== "none") {
+                
+            // }
+            current_pie = selected;
+            // console.log(layers);
+        })
+        map.removeLayer(pies)
+    })
+    map.on("layeradd", function(e) {
+        // Handle only marker layers
+        current_pie = e.layer.options.value;
+        // console.log(e.layer.options.value);
+        if((e.layer.options.id != "markerLayer1") && (e.layer.options.id != "markerLayer2")) {
+        return;
+      }
+    
+      // For the currently added layer (which is one with markers)
+      // get all its layers
+      var markers = e.layer.getLayers();
+    
+
+      
+      
+    });
+
+    
+    
 }
 
 
@@ -65,12 +164,12 @@ function createClusters(airbnbData) {
 
 d3.json(neighborhoodsURL, function (neighborhoodData) {
     d3.json("/data/airbnb", function(airbnbData) {
-        // d3.json("/data/airbnb_rooms")
-        // console.log(neighborhoodData);
-        createFeatures(neighborhoodData);
-        // console.log(airbnbData);
+        d3.json("/data/airbnb_rooms", function(roomData) {
         
-        createMap(neighborhoodData,airbnbData);
+            createFeatures(neighborhoodData);
+        
+            createMap(neighborhoodData,airbnbData,roomData);
+        })
     })
         
 })
